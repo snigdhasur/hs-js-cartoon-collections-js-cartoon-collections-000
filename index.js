40cbb8c92eb124d20fc4eaec0b9c4e9e2e403dad
Snigdha Sur <sur.snigdha@gmail.com>
2017-05-29 14:54:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(array) {
+  var string = "";
+  for(let i=0; i < array.length; i++) {
+  string += `${i+1}. ${array[i]} `;
+}
+ return string
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(array){
+  var arrayModified = [];
+  for(let i=0; i < array.length ; i++) {
+  arrayModified.push(`${array[i].toUpperCase()}!`);
+}
+ return arrayModified
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(array) {
+  for(let i=0; i < array.length ; i++) {
+    if (array[i].length < 5) {
+      return false
+    }
+    else {
+      return true
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (array) {
+  for(let i=0; i < array.length ; i++) {
+    if (array[i] === 'camembert' || array[i] === 'gouda' || array[i] === 'cheddar') {
+      return array[i]
+    }
+    }
 }
